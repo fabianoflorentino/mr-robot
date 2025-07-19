4bd55cced37ef393cc5d144942385efc7a626911
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-07-19 12:04:41 -0300

    feat: add PaymentRepository interface for processing payments

---
+++ core/repository/payment_repository.go
@@ -0,0 +1,11 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/fabianoflorentino/mr-robot/core/domain"
+)
+
+type PaymentRepository interface {
+	Process(ctx context.Context, payment *domain.Payment) error
+}
