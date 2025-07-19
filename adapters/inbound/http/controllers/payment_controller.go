353435a74ba3e043732c22efdf61a33c6a7ac212
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-07-19 17:40:43 -0300

    feat: implement PaymentController for processing payments

---
+++ adapters/inbound/http/controllers/payment_controller.go
@@ -0,0 +1,35 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/fabianoflorentino/mr-robot/adapters/inbound/http/handlers"
+	"github.com/fabianoflorentino/mr-robot/core/domain"
+	"github.com/fabianoflorentino/mr-robot/core/services"
+	"github.com/gin-gonic/gin"
+)
+
+type PaymentController struct {
+	srv *services.PaymentService
+}
+
+func NewPaymentController(p *services.PaymentService) *PaymentController {
+	return &PaymentController{srv: p}
+}
+
+func (u *PaymentController) ProcessPayment(c *gin.Context) {
+	var payment = &domain.Payment{}
+
+	if err := handlers.ShouldBindJSON(c, &payment); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request, amount is required"})
+		return
+	}
+
+	payment, err := u.srv.Process(c, payment)
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Payment processed"})
+}
