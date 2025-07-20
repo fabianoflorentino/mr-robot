17908eda06bccb52f317bcc3099214a48fdb9707
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-07-20 17:49:18 -0300

    feat: add payment queue

---
+++ internal/app/queue/payment_queue.go
@@ -0,0 +1,46 @@
+package queue
+
+import (
+	"context"
+	"log"
+
+	"github.com/fabianoflorentino/mr-robot/core/domain"
+	"github.com/fabianoflorentino/mr-robot/core/services"
+)
+
+type PaymentJob struct {
+	Payment *domain.Payment
+}
+
+type PaymentQueue struct {
+	jobs    chan PaymentJob
+	workers int
+	service *services.PaymentService
+}
+
+func NewPaymentQueue(workers int, service *services.PaymentService) *PaymentQueue {
+	q := &PaymentQueue{
+		jobs:    make(chan PaymentJob, 100),
+		workers: workers,
+		service: service,
+	}
+
+	for j := 0; j < workers; j++ {
+		go q.worker(context.Background())
+	}
+
+	return q
+}
+
+func (q *PaymentQueue) Enqueue(payment *domain.Payment) {
+	q.jobs <- PaymentJob{Payment: payment}
+}
+
+func (q *PaymentQueue) worker(ctx context.Context) {
+	for job := range q.jobs {
+		_, err := q.service.Process(ctx, job.Payment)
+		if err != nil {
+			log.Printf("Failed to process payment: %v", err)
+		}
+	}
+}
