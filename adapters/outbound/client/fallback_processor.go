fab5741cd91631e1ac07be5bc6587a1002a1f080
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-07-20 17:50:23 -0300

    feat: add function proccess fallback

---
+++ adapters/outbound/client/fallback_processor.go
@@ -0,0 +1,13 @@
+package client
+
+import "github.com/fabianoflorentino/mr-robot/core/domain"
+
+type ProcessorFallbackClient struct{}
+
+func (p *ProcessorFallbackClient) Fallback() string {
+	return "fallback"
+}
+
+func (p *ProcessorFallbackClient) Process(payment *domain.Payment) (bool, error) {
+	return true, nil
+}
