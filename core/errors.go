23d9d2e8dfa743c65146b533c1d35175ab1ae387
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-07-28 00:32:27 -0300

    feat: new error message

--- core/errors.go
+++ core/errors.go
@@ -3,5 +3,6 @@ package core
 import "errors"
 
 var (
-	ErrPaymentNotProcessed = errors.New("payment can't be processed")
+	ErrPaymentNotProcessed     = errors.New("payment can't be processed")
+	ErrPaymentProcessingFailed = errors.New("payment processing failed")
 )
