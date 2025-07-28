ec9682af2f4d884cf9fe1249aca960fd12e4a3d1
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-07-28 09:25:43 -0300

    feat: refactor PaymentService to include processor and improve payment processing flow

--- core/services/payment_service.go
+++ core/services/payment_service.go
@@ -3,22 +3,33 @@ package services
 import (
 	"context"
 
+	"github.com/fabianoflorentino/mr-robot/core"
 	"github.com/fabianoflorentino/mr-robot/core/domain"
 	"github.com/fabianoflorentino/mr-robot/core/repository"
 )
 
 type PaymentService struct {
-	payment repository.PaymentRepository
+	repo      repository.PaymentRepository
+	processor domain.PaymentProcessorDefault
 }
 
-func NewPaymentService(p repository.PaymentRepository) *PaymentService {
-	return &PaymentService{payment: p}
+func NewPaymentService(r repository.PaymentRepository, p domain.PaymentProcessorDefault) *PaymentService {
+	return &PaymentService{repo: r, processor: p}
 }
 
-func (p *PaymentService) Process(ctx context.Context, payment *domain.Payment) (*domain.Payment, error) {
-	if err := p.payment.Process(ctx, payment); err != nil {
-		return nil, err
+func (s *PaymentService) Process(ctx context.Context, payment *domain.Payment) error {
+	ok, err := s.processor.Process(payment)
+	if err != nil {
+		return err
 	}
 
-	return payment, nil
+	if !ok {
+		return core.ErrPaymentProcessingFailed
+	}
+
+	if err := s.repo.Process(ctx, payment); err != nil {
+		return err
+	}
+
+	return nil
 }
