9efd4aa9e7f7012245f1a336b8bbbf5c5ba2bb9f
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-07-28 09:25:35 -0300

    feat: add PaymentProcessor interfaces for processing payments

--- core/domain/payment.go
+++ core/domain/payment.go
@@ -6,3 +6,13 @@ type Payment struct {
 	CorrelationID uuid.UUID `json:"correlation_id" binding:"required"`
 	Amount        float64   `json:"amount" binding:"required,gt=0"`
 }
+
+type PaymentProcessorDefault interface {
+	Process(payment *Payment) (bool, error)
+	Default() string
+}
+
+type PaymentProcessorFallback interface {
+	Process(payment *Payment) (bool, error)
+	Fallback() string
+}
