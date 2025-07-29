cadb3ff762878c3a958545ce8ad15919cdcb002e
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-07-28 23:55:15 -0300

    refactor: improve LoadEnv function to conditionally skip .env file loading for tests

--- config/config.go
+++ config/config.go
@@ -3,11 +3,18 @@ package config
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() error {
+	// Skip loading .env file if SKIP_ENV_FILE is set (useful for tests)
+	if os.Getenv("SKIP_ENV_FILE") == "true" {
+		fmt.Println("Skipping .env file loading (SKIP_ENV_FILE=true)")
+		return nil
+	}
+
 	if err := godotenv.Load("config/.env"); err != nil {
 		log.Fatalf("error: to load env config: %v", err)
 	}
