94a2646e290eacb8dca5b2aa367328c2a7eb2157
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-07-31 00:29:40 -0300

    feat: add timestamps to Payment model for tracking creation and updates

--- adapters/outbound/persistence/data/payment_model.go
+++ adapters/outbound/persistence/data/payment_model.go
@@ -1,9 +1,15 @@
 package data
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Payment struct {
 	ID            uuid.UUID `gorm:"primaryKey,type:uuid;default:gen_random_uuid()"`
 	CorrelationID uuid.UUID `gorm:"not null"`
 	Amount        float64   `gorm:"not null"`
+	CreatedAt     time.Time `gorm:"autoCreateTime"`
+	UpdatedAt     time.Time `gorm:"autoUpdateTime"`
 }
