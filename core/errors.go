46fcb7f11ebd5f256db3431fa4d87690dc4c5254
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-07-31 00:01:11 -0300

    feat: add error for full payment queue

--- core/errors.go
+++ core/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrPaymentNotProcessed     = errors.New("payment can't be processed")
 	ErrPaymentProcessingFailed = errors.New("payment processing failed")
+	ErrQueueFull               = errors.New("payment queue is full")
 )
