9fee91df47900d4fb5a2ab4bca3f8c5b90c66755
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-08-02 06:44:26 -0300

    fix: ensure Processor field is marked as not null in Payment model

--- adapters/outbound/persistence/data/payment_model.go
+++ adapters/outbound/persistence/data/payment_model.go
@@ -10,6 +10,7 @@ type Payment struct {
 	ID            uuid.UUID `gorm:"primaryKey,type:uuid;default:gen_random_uuid()"`
 	CorrelationID uuid.UUID `gorm:"not null"`
 	Amount        float64   `gorm:"not null"`
+	Processor     string    `gorm:"not null"`
 	CreatedAt     time.Time `gorm:"autoCreateTime"`
 	UpdatedAt     time.Time `gorm:"autoUpdateTime"`
 }
