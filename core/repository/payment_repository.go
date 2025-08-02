e30963a0d83081ee759caa3dbc2d125222f6cab3
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-08-02 11:41:11 -0300

    feat: add Summary method to PaymentRepository for payment summary retrieval

--- core/repository/payment_repository.go
+++ core/repository/payment_repository.go
@@ -2,10 +2,12 @@ package repository
 
 import (
 	"context"
+	"time"
 
 	"github.com/fabianoflorentino/mr-robot/core/domain"
 )
 
 type PaymentRepository interface {
 	Process(ctx context.Context, payment *domain.Payment, processorName string) error
+	Summary(ctx context.Context, from, to *time.Time) (*domain.PaymentSummary, error)
 }
