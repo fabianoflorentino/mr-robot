4d7df159e17221bd066a59e4f809d991c0fe0455
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-08-02 11:41:24 -0300

    feat: add PaymentSummary and ProcessorSummary types for payment processing metrics

--- core/domain/payment.go
+++ core/domain/payment.go
@@ -7,6 +7,16 @@ type Payment struct {
 	Amount        float64   `json:"amount" binding:"required,gt=0"`
 }
 
+type PaymentSummary struct {
+	Default  ProcessorSummary `json:"default"`
+	Fallback ProcessorSummary `json:"fallback"`
+}
+
+type ProcessorSummary struct {
+	TotalRequests int64   `json:"totalRequests"`
+	TotalAmount   float64 `json:"totalAmount"`
+}
+
 type PaymentProcessor interface {
 	Process(payment *Payment) (bool, error)
 	ProcessorName() string
