4be3feb46e0f588b1fc76326ffa0d09ca48bdb45
Fabiano Santos Florentino <fabianoratm@gmail.com>
2025-08-13 16:36:09 -0300

    feat:  HealthCheckController to use ConfigManager for configuration loading

--- adapters/inbound/http/controllers/healthcheck_controller.go
+++ adapters/inbound/http/controllers/healthcheck_controller.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/fabianoflorentino/mr-robot/internal/app/controller"
 )
 
 type HealthCheckController struct{}
@@ -12,17 +14,23 @@ func NewHealthCheckController() *HealthCheckController {
 }
 
 func (h *HealthCheckController) HealthCheck(w http.ResponseWriter, r *http.Request) {
-	cfg := loadControllerConfig()
+	configManager := controller.NewConfigManager()
+	err := configManager.LoadConfig()
+	if err != nil {
+		http.Error(w, "Error loading configuration", http.StatusInternalServerError)
+		return
+	}
+	cfg := configManager.GetConfig()
 
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
-	response := map[string]any{"service": cfg.ControllerConfig.HostName, "time": cfg.ControllerConfig.TimeInfo}
+	response := map[string]any{"service": cfg.HostName, "status": http.StatusOK}
 
-	w.Header().Set(cfg.ControllerConfig.ContentType, cfg.ControllerConfig.ApplicationJSON)
-	w.WriteHeader(cfg.ControllerConfig.StatusOK)
+	w.Header().Set(cfg.ContentType, cfg.ApplicationJSON)
+	w.WriteHeader(http.StatusOK)
 
 	if err := json.NewEncoder(w).Encode(response); err != nil {
 		http.Error(w, "Error encoding JSON response", http.StatusInternalServerError)
